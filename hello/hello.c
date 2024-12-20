ff3c700e69869cb648a865dae028e2f165a72680
edoardosicuro <edoardosicuro@gmail.com>
2024-12-20 16:09:51 +0000

    aggiunta scanf e printf

--- hello/hello.c
+++ hello/hello.c
@@ -9,8 +9,21 @@ int main()
     // \n is the shortcut for new line
    
     printf("Hello world!\n");
+
+    // variable of 32 chars 
+    char name[32];
+
+    // ask for a name
+    printf(" Enter a name: ");
+
+    // read the input
+    scanf("%s",name);
+
+    // print a message with the name entered
+    printf("\nHello\t%s\n\n\n",name);
+
     
-    /* return 0: indicated that tyhe program is successfull*/
+    /* return 0: indicated that the program is successfull*/
 
     return 0;
 }
